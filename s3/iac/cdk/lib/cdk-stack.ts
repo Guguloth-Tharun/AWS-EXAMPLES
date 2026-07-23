be2c9feb2fc860b77be4637f6dc7a43fd7be0e21
Guguloth-Tharun <100685900+Guguloth-Tharun@users.noreply.github.com>
2026-07-23 18:03:59 +0000

    Add AWS CDK and Node.js to devcontainer

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,19 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+// https://github.com/aws/aws-cdk/blob/main/README.md
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this,'MyBucket');
+    
+  }
+}
+
+// cdk synth
+// cdk deploy
+// cdk destroy
+
